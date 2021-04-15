c878dc6d1016aea1301b5c68d631dba879f51122
Swaroop <breadfan18@gmail.com>
2021-04-15 17:39:35 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    dob: Date,
+    city: String,
+    state: String    
+})
+
+module.exports = mongoose.model('User', usersSchema);
