a08a359641c41700e8cb55ae55fc785c1c3010e7
Swaroop <breadfan18@gmail.com>
2021-04-14 22:42:16 -0400

    added basic server.js code

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import required 
+const express = require('express');
+const PORT = 3002;
+
+const indexRouter = require('.routes/index');
+const morgan = require('morgan');
+
+
+// initialize the express app
+const app = express();
+
+// connect to the DB
+require('./config/database');
+
+// Configure the view engine
+app.set('view engine', 'ejs');
+
+// Mount middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+// Mount the routers 
+app.use('/', indexRouter);
+
+
+// Tell the app to listen
+app.listen(PORT, function () {
+    console.log(`Express is listening on port: ${PORT}`);
+})
