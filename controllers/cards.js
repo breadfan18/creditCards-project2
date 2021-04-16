97585424cf2a2cb4e52153ad60a353aabd04bf72
Swaroop <breadfan18@gmail.com>
2021-04-16 17:46:58 -0400

    cardsCtrl.create funciton is complete. data is being saved in mongo

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,5 +15,27 @@ function newCard(req, res) {
 }
 
 function create(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
+
+    let creditPullArr = [req.body.experian, req.body.equifax, req.body.transunion];
+
+    let cardObj = {
+        applicant: req.body.applicant,
+        issuer: req.body.issuer,
+        cardName: req.body.cardName,
+        appDate: req.body.appDate,
+        creditPull: creditPullArr,
+        nextFeeDate: req.body.nextFeeDate,
+        creditLine: req.body.creditLine,
+        bonusSpend: req.body.bonusSpend,
+        bonusSpendDate: req.body.bonusSpendDate,
+        annualFee: req.body.annualFee
+    }
+
+    Card.create(cardObj, function(err, card){
+        console.log(err)
+        if(err) return res.redirect('/cards/new');
+        res.redirect('/cards');
+    })
+   
 }
