56defebf85ce4f19d82658a448b311c70509f67d
Swaroop <breadfan18@gmail.com>
2021-04-16 20:46:34 -0400

    users create is completed

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports = {
-    new: addNewUser
+    new: addNewUser,
+    create
 }
 
 function addNewUser(req, res) {
     res.render('users/new');
 }
 
+function create(req, res) {
+    User.create(req.body, function(err, user){
+        if(err) {
+            console.log('could not add user');
+            console.log(err);
+        }
+        res.redirect('/users/new');
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersCtrl = require('../controllers/users');
 
 router.get('/new', usersCtrl.new);
+router.post('/new', usersCtrl.create);
 
 module.exports = router;
 
