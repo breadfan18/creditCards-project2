864c21787c44df4c5a9e10ab355cb3f1ecb90cd3
Swaroop <breadfan18@gmail.com>
2021-04-27 23:41:13 -0400

    added routes for google auth flow

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const Admin = require('../models/admin');
 
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
@@ -7,8 +8,31 @@ passport.use(new GoogleStrategy({
         callbackURL: process.env.GOOGLE_CALLBACK
     },
     function (accessToken, refreshToken, profile, cb) {
-        
+        Admin.findOne({'googleId': profile.id}, function (err, admin) {
+            if (err) return cb(err);
+            if (admin){
+                return cb(null, admin);
+            } else {
+                const newAdmin = new Admin({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id
+                });
+                newAdmin.save(function (err) {
+                    if(err) return cb(err);
+                    return cb(null, newAdmin);
+                });
+            }
+        });
     }
+));
 
+passport.serializeUser(function (admin, done) {
+    done(null, admin.id)
+})
 
-))
+passport.deserializeUser(function (id, done) {
+    Admin.findById(id, function (err, admin) {
+        done(err, admin);
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const router = require('express').Router();
 const indexCtrl = require('../controllers/index');
-const router = express.Router();
+const passport = require('passport');
 
 // Router to the root route. that is localhost:3002
 router.get('/', indexCtrl.index);
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/oauth2callback', passport.authenticate('google', {
+    successRedirect: '/cards',
+    failureRedirect: '/'
+}));
 
 module.exports = router;
